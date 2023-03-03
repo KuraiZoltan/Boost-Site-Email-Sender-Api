c799c06925e53e7a5e7b35242e84a66428626d12
KuraiZoltan <kz1022@gmail.com>
2023-03-03 20:54:38 +0100

    Add project files.

---
+++ Controllers/EmailController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace EmailSender.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult SendEmail(string body)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse("fakebboost@gmail.com"));
+            email.To.Add(MailboxAddress.Parse("kz1022@gmail.com"));
+            email.Subject = "Test Email Subject";
+            email.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = body };
+
+            using var smtp = new SmtpClient();
+            smtp.Connect("smtp.gmail.com", 465, MailKit.Security.SecureSocketOptions.SslOnConnect);
+            smtp.Authenticate("fakebboost@gmail.com", "gconlzshnpusjupa");
+            smtp.Send(email);
+            smtp.Disconnect(true);
+
+            return Ok();
+        }
+    }
+}
