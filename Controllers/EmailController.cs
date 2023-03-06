ef9e6d412fab633be44b7a49ca1b29d8cd63fee4
KuraiZoltan <kz1022@gmail.com>
2023-03-06 15:18:05 +0100

    EmailController update with frontend request

--- Controllers/EmailController.cs
+++ Controllers/EmailController.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using MailKit.Net.Smtp;
 using MimeKit;
+using EmailSender.Models;
 
 namespace EmailSender.Controllers
 {
@@ -10,13 +10,22 @@ namespace EmailSender.Controllers
     public class EmailController : ControllerBase
     {
         [HttpPost]
-        public IActionResult SendEmail(string body)
+        public IActionResult SendEmail([FromBody] OrderDetails orderDetails)
         {
             var email = new MimeMessage();
             email.From.Add(MailboxAddress.Parse("fakebboost@gmail.com"));
-            email.To.Add(MailboxAddress.Parse("kz1022@gmail.com"));
-            email.Subject = "Test Email Subject";
-            email.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = body };
+            email.To.Add(MailboxAddress.Parse(orderDetails.Email));
+            email.Subject = "FB Order Confirmation";
+            email.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text =
+                $"<h1>FB Order Details</h1> " +
+                $"<p>First Name: {orderDetails.FirstName}</p> " +
+                $"<p>Last Name: {orderDetails.LastName}</p> " +
+                $"<p>Boost From: {orderDetails.CurrentRank} {orderDetails.CurrentRankLevel}</p> " +
+                $"<p>Boost To: {orderDetails.OrderedRank} {orderDetails.OrderedRankLevel}</p>" +
+                "<h1>Thank You for your order, FB will contact you soon!</h1>"
+            };
 
             using var smtp = new SmtpClient();
             smtp.Connect("smtp.gmail.com", 465, MailKit.Security.SecureSocketOptions.SslOnConnect);
